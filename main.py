57aee604c8e5d5898ecadd4c309a07f5cd25d6d4
SarupaVL <145839268+SarupaVL@users.noreply.github.com>
2024-04-12 18:54:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,3 @@
-#remember that this is a menu driven program
-
-""" import typer
-app=typer.Typer()
-
-@app.command()
-def hello():
-    print("helloooo")
-
-@app.command()
-def goodbye():
-    print("Goodbye")
-
-if __name__=="__rept__":
-    app() """
-
-""" def main():
-
-    #initializing an empty kanban boards' list
-    boards=[]
-    todo={"Task name":[],"Description":[],"Assignees":[],"Reporters":[],"Priority":[]}
-    doing={"Task name":[],"Description":[],"Assignees":[],"Reporters":[],"Priority":[]}
-    done={"Task name":[],"Description":[],"Assignees":[],"Reporters":[],"Priority":[]}
-
-    choice=0
-    while choice!=5:
-        print("1. Create a new kanban board")
-        print("2. Edit existing kanban board")
-        print("3. Delete kanban board")
-        print("4. Display a kanban board")
-        print("5. Exit")
-        choice=int(input("Enter your choice:"))
-
-        if choice==1:
-            print("Creating a new kanban board...")
-            kb=input("Enter name of the new board")
-            boards.append(kb)
-
-            print("Lets add your tasks in their respective boxes!")
-            dywc=""
-            while dywc!="no":
-                tname=input("Enter task name: ")
-                tstat=input("Todo/Doing/Done: ")
-                tdesc=input("Enter task description: ")
-                tassi=input("Enter assignees: ")
-                trepo=input("Enter reporters: ")
-                tprio=input("Enter priority (high,medium,low): ")
-                if tstat=="Todo":
-                    todo["Task name"]+=[tname]
-                    todo["Description"]+=[tdesc]
-                    todo["Assignees"]+=[tassi]
-                    todo["Reporters"]+=[trepo]
-                    todo["Priority"]+=[tprio]
-                elif tstat=="Doing":
-                    doing["Task name"]+=[tname]
-                    doing["Description"]+=[tdesc]
-                    doing["Assignees"]+=[tassi]
-                    doing["Reporters"]+=[trepo]
-                    doing["Priority"]+=[tprio]
-                elif tstat=="Done":
-                    done["Task name"]+=[tname]
-                    done["Description"]+=[tdesc]
-                    done["Assignees"]+=[tassi]
-                    done["Reporters"]+=[trepo]
-                    done["Priority"]+=[tprio]
-                dywc=input("Do you wanna continue adding tasks? (yes/no): ")
-
-        if choice==4:
-            print(todo)
-            print(doing)
-            print(done)
-
-if __name__=="__main__":
-    main() """
-
-
 class Task:
     def __init__(self, name, description, assignees, reporters, priority):
         self.name = name
@@ -97,6 +21,25 @@ class KanbanBoard:
         elif status == "Done":
             self.done.append(task)
 
+    def delete_task(self, task_name, status):
+        if status == "Todo":
+            for task in self.todo:
+                if task.name == task_name:
+                    self.todo.remove(task)
+                    return True
+        elif status == "Doing":
+            for task in self.doing:
+                if task.name == task_name:
+                    self.doing.remove(task)
+                    return True
+        elif status == "Done":
+            for task in self.done:
+                if task.name == task_name:
+                    self.done.remove(task)
+                    return True
+        return False
+            
+
 def main():
     boards = []
 
@@ -115,8 +58,37 @@ def main():
             new_board = KanbanBoard(kb_name)
             boards.append(new_board)
 
-            print("Lets add your tasks in their respective boxes!")
+            print("Lets add your tasks in their respective boxes!!!")
             add_tasks(new_board)
+
+        if choice == 2:
+            choicea = 0
+            while choicea!=3:
+                print("1. Adding a task")
+                print("2. Deleting a task")
+                print("3. Exit")
+                choicea=int(input("Enter your choice: "))
+
+                kb_namee = input("Enter name of the kanban board to be edited: ")
+
+                for board in boards:
+                    if board.name == kb_namee:
+                        if choicea == 1:
+                            board.add_task()
+                        elif choicea == 2:
+                            board.delete_task()
+                        elif choicea == 3:
+                            break
+                        else:
+                            print("Invalid choice!")
+                        break
+                else:
+                    print("Kanban board not found!")
+
+        if choice == 3:
+            print("Deleting a kanban board...")
+            kb_nameee=input("Enter the name of kanban board to be deleted: ")
+            boards.remove("kb_nameee")
 
         if choice == 4:
             for board in boards:
